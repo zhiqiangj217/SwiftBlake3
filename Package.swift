2d0936ef5fbe6a5442ade643435c7c219e970479
jiangzhiqiang <zhiqiangj217@gmail.com>
2026-01-05 01:53:12 +0800

    update package name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-blake3",
+    name: "SwiftBlake3",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
